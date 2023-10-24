84725ab2391948a59ce9b3cf7c8d262deab9705e
Mark Lyon <damionfury@gmail.com>
2023-10-24 18:00:50 -0500

    Add string decode and move to better testing pattern

--- b64.py
+++ b64.py
@@ -1,6 +1,4 @@
-import sys
-
-inputString = "Hello World"
+import sys, base64
 
 capitalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
@@ -35,7 +33,6 @@ def base64Encode(string):
         # Something went really wrong and we should stop
         print("Binary string is the wrong length. This should not happen.")
         print("String length: " + str(len(binaryString)))
-        print(binaryString)
         sys.exit("Error: Bad length binary string")
 
     for i in range(0,len(binaryString),6):
@@ -45,4 +42,55 @@ def base64Encode(string):
         
     return outputString + pad
 
-print(base64Encode(inputString))
+def base64Decode(string):
+    output = ""
+    binaryString = ""
+    
+    for char in string:
+        if char != "=":
+            dec = base64Chars.index(char)
+            binaryString += f'{dec:06b}'
+        else:
+            binaryString = binaryString[0:len(binaryString)-2]
+    
+    if len(binaryString) % 8 > 0:
+        # Length should be evenly divisible by 8
+        print("Decoded binary is the wrong length. This should not happen.")
+        print("String length: " + str(len(binaryString)))
+        sys.exit("Error: Bad length binary string")
+        
+    for i in range(0,len(binaryString),8):
+        bin = binaryString[i:i+8]
+        dec = int(bin,2)
+        output += chr(dec)
+        
+    return output
+
+def testEncode(string):
+    
+    correctEncode = base64.b64encode(string.encode()).decode("utf-8")
+    test = base64Encode(string)
+    
+    if test != correctEncode:
+        print("Encode test failed.")
+        print("Correct: " + correctEncode)
+        print("Test: " + test)
+    else:
+        print("Encode test passed.")
+        
+def testDecode(string):
+    
+    correctDecode = base64.b64decode(string.encode()).decode("utf-8")
+    test = base64Decode(string)
+    
+    if test != correctDecode:
+        print("Decode test failed.")
+        print("Correct: " + correctDecode)
+        print("Test: " + test)
+    else:
+        print("Decode test passed.")
+
+testText = "Hello World"
+testB64 = "SGVsbG8gV29ybGQ="
+
+testDecode(testB64)
