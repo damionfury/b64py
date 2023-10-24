0a79afd4e6fafe5c49e7e6007fc4720f2b28a34f
Mark Lyon <damionfury@gmail.com>
2023-10-24 17:11:07 -0500

    Initial commit with text encoder

---
+++ README.md
@@ -0,0 +1,17 @@
+# Reimplementing base64 encode and decode
+I learn better when I have a goal in mind. After seeing an article about how base64 encode and decode functions work, I got it in my head to write my own implementation in python.
+
+## Goal
+Create a module that offers base64 encode and decode
+
+## Roadmap
+1. Make a function that can base64 encode a string
+2. Make a function that can base64 decode a string
+3. Modify the functions to work with any data
+4. Turn it into a module that can be imported.
+
+## Where am I at?
+Step 1 is done.
+
+## Can I use this?
+Yes, but why? There is no rational reason for you to use this. Still – if you're dead set on making bad decisions – it's licensed CC0. I'd kinda prefer you left my name out of it.
---
+++ b64.py
@@ -0,0 +1,48 @@
+import sys
+
+inputString = "Hello World"
+
+capitalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
+digits = "0123456789"
+symbols = "+/"
+
+base64Chars = capitalChars + lowercaseChars + digits + symbols
+
+def base64Encode(string):
+    outputString = ""
+    binaryString = ""
+    pad = ""
+
+    # Convert to a long string of binary
+    for char in string:
+        binaryString += f'{ord(char):08b}'
+        
+    # Now determine if padding is needed
+    remainder = len(binaryString) % 6
+
+    # Wish python3.9 had switch case but maybe I'll move to 3.10 at some point
+
+    if remainder == 4:
+        # Need two more zeroes
+        binaryString += "00"
+        pad = "="
+    elif remainder == 2:
+        # Need four more zeroes
+        binaryString += "0000"
+        pad = "=="
+    elif remainder % 2 == 1 :
+        # Something went really wrong and we should stop
+        print("Binary string is the wrong length. This should not happen.")
+        print("String length: " + len(binaryString))
+        print(binaryString)
+        sys.exit("Error: Bad length binary string")
+
+    for i in range(0,len(binaryString),6):
+        bin = binaryString[i:i+6]
+        dec = int(bin,2)
+        outputString += base64Chars[dec]
+        
+    return outputString + pad
+
+print(base64Encode(inputString))
